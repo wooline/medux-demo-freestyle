4696bc2fd534f499eba28ce66b68ab8edf1c5c3e
星矢 <zhaoyong1@akulaku.com>
2021-01-21 11:16:37 +0800

    调整目录

---
+++ react-redux-taro/src/global.d.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+declare const process: {
+  env: {
+    TARO_ENV:
+      | 'weapp'
+      | 'swan'
+      | 'alipay'
+      | 'h5'
+      | 'rn'
+      | 'tt'
+      | 'quickapp'
+      | 'qq'
+      | 'jd';
+    [key: string]: any;
+  };
+};
